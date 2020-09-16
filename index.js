32bbac8899bea71ac9a356462a6231d3e5467003
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-16 19:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ function placeOrder(cardNumber) {
   if(cardNumber===undefined){
     return("Sorry, we don't have a credit card on file for you.");
   }else {
-    var total= total();
+    var t= total();
     cart=[];
-    return(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+    return(`Your total cost is $${t}, which will be charged to the card ${cardNumber}.`);
   }
 }
